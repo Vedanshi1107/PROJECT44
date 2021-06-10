1b4be72d19834b2dbc2bffec536b65b71c5a1509
Vedanshi1107 <2335.stkabirnrnp@gmail.com>
2021-06-10 17:25:04 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,24 @@
+gameName-STREET DOG
+
+pc- dog 
+npc-trashcans,dustbins,houseowner,food;
+
+story - the dog was stealing food from the bin and the houseowner saw it so he ran behind the dog to catch him.
+
+goal - not to get caught by the owner.
+
+rules - 1.not to go out of the track
+        2.make sure you dont hit the obstcale
+        3.use the space key to jump on the obstcale 
+        4.use the arrow keys to move to other tracks
+        5.you only have one chance to escape
+
+score - score can be increased by eating the food on the tracks.
+
+chances - random
+
+feedback - if you eat any food and when the houseowner is running behind.
+
+balance - medium
+
+
